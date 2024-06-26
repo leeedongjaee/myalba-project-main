ad16fc7213ade040b072ee059ed9d68d163450a5
dongjae <harold306122@gmail.com>
2024-06-26 22:18:35 +0900

    브랜드 repository

--- src/main/java/hello/hellospring/repository/BrandRepository.java
+++ src/main/java/hello/hellospring/repository/BrandRepository.java
@@ -13,4 +13,5 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
     Optional<Brand> findById(Long id);//브랜드 아이디를 통해 브랜드 찾는 메서드
     @Query("SELECT b FROM Brand b ORDER BY b.averageRating DESC")
     List<Brand> findAllOrderByAverageRatingDesc();//브랜드 평점 평균 높은 순서로 불러오는 메서드
+
 }
