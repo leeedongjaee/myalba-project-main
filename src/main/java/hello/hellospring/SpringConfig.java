44dd1912065969372bcf83b8f8c96bc9654b64dc
dongjae <harold306122@gmail.com>
2024-06-26 22:22:17 +0900

    SpringConfig

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,7 +6,6 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class SpringConfig {
     private final MemberRepository memberRepository;
