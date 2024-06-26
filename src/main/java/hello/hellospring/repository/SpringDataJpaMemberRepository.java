8fcb9ece01958f489abf6e6930b3ca3a5ce9222a
dongjae <harold306122@gmail.com>
2024-06-26 22:20:03 +0900

    스프링 repository

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>,MemberRepository {
-
     @Override
     Optional<Member> findByName(String name);//이름을 통해 찾는 메서드
 }
