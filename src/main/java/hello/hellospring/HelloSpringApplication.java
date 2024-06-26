a3dd7651086002ec94c355f077afbd0d192d9f97
dongjae <harold306122@gmail.com>
2024-06-26 22:22:10 +0900

    main

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,7 +2,6 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HelloSpringApplication {
 
