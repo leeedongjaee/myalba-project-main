080dffdbf9d940737ca6494eebcd0e71df03f61b
dongjae <harold306122@gmail.com>
2024-06-26 22:18:51 +0900

    채용공고 repository

--- src/main/java/hello/hellospring/repository/HirePostRepository.java
+++ src/main/java/hello/hellospring/repository/HirePostRepository.java
@@ -4,4 +4,5 @@ import hello.hellospring.domain.HirePost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HirePostRepository extends JpaRepository<HirePost, Long> {
+
 }
