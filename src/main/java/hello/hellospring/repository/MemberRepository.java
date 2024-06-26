0ee4ea8222da78227c9f545c070bbbcd888381f2
dongjae <harold306122@gmail.com>
2024-06-26 22:19:09 +0900

    회원 repository

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -11,7 +11,6 @@ public interface MemberRepository {
     Optional<Member> findByName(String name);//이름을 통해 찾는 메서드
     Optional<Member> findByEmail(String email);//이메일을 통해 찾는 메서드
     Optional<Member> findByNickname(String nickname);//닉네임을 통해 찾는 메서드
-
     List<Member> findAll();//모든 회원을 불러오는 메서드
 
 }
